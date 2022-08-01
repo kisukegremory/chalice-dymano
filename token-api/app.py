835c628415a397b410e8aa3634b55ed853d2d277
Gustavo de Souza Gonçalves <goncalves.gustavo211@gmail.com>
2022-08-01 18:50:55 -0300

    feat(app): added a raw token validation

--- token-api/app.py
+++ token-api/app.py
@@ -1,12 +1,21 @@
-from chalice import Chalice
+from chalice import Chalice, AuthResponse
 import json
 import boto3
+
+
+
 
 app = Chalice(app_name='token-api-chalice')
 s3 = boto3.client('s3')
 
-@app.route('/')
-def index():
+@app.authorizer()
+def myauth(event):
+    if event.token == 'allow':
+        return AuthResponse(['*'], principal_id='id')
+    return AuthResponse([], principal_id='user')
+
+@app.route('/googlesheets',methods=['POST'], authorizer=myauth)
+def googlesheets():
     bucket = 's3-test-googlesheets'
     key = 'credentials.json'
     
@@ -20,5 +29,5 @@ def index():
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps(json_obj)
+        'body': json_obj
     }
