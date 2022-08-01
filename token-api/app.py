a9247b7aeacc9a73cdf91c7d01f7ca01c6cb60ed
Gustavo de Souza Gonçalves <goncalves.gustavo211@gmail.com>
2022-08-01 17:42:48 -0300

    feat(app): added the s3 reader

--- token-api/app.py
+++ token-api/app.py
@@ -1,29 +1,24 @@
 from chalice import Chalice
+import json
+import boto3
 
 app = Chalice(app_name='token-api-chalice')
-
+s3 = boto3.client('s3')
 
 @app.route('/')
 def index():
-    return {'hello': 'world'}
-
-
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+    bucket = 's3-test-googlesheets'
+    key = 'credentials.json'
+    
+    res = s3.get_object(Bucket=bucket, Key=key)
+    print(res)
+    content = res['Body']
+    
+    json_obj = json.loads(content.read())
+    
+    
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps(json_obj)
+    }
