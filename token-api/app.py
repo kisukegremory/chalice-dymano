372d9e1c96674b28d8789979a908830f92e0a495
Gustavo de Souza Gonçalves <goncalves.gustavo211@gmail.com>
2022-08-03 10:03:33 -0300

    feat(app): added route using dynamo as a source

--- token-api/app.py
+++ token-api/app.py
@@ -7,6 +7,7 @@ import boto3
 
 app = Chalice(app_name='token-api-chalice')
 s3 = boto3.client('s3')
+dynamodb = boto3.resource('dynamodb', region_name='sa-east-1')
 
 @app.authorizer()
 def myauth(event):
@@ -14,7 +15,7 @@ def myauth(event):
         return AuthResponse(['*'], principal_id='id')
     return AuthResponse([], principal_id='user')
 
-@app.route('/googlesheets',methods=['POST'], authorizer=myauth)
+@app.route('/s3',methods=['POST'], authorizer=myauth)
 def googlesheets():
     bucket = 's3-test-googlesheets'
     key = 'credentials.json'
@@ -25,6 +26,19 @@ def googlesheets():
     
     json_obj = json.loads(content.read())
     
+    
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json_obj
+    }
+
+
+@app.route('/dynamo',methods=['POST'], authorizer=myauth)
+def by_dynamo():
+    table = dynamodb.Table('googlesheets')
+    res = table.get_item(Key={'token': 'googlesheets'})
+    json_obj = res['Item']['json']
     
     # TODO implement
     return {
