cccd7492f7bcad542470a9a4a541a2631a5b1408
Gustavo de Souza Gonçalves <goncalves.gustavo211@gmail.com>
2022-08-09 20:15:15 -0300

    feat(json-to-dynamo): added cli to project

--- json-to-dynamo/main.py
+++ json-to-dynamo/main.py
@@ -1,13 +1,15 @@
 
+import click
 import boto3
 import json
+from time import sleep
 
-dynamodb = boto3.resource('dynamodb', region_name='sa-east-1')
-table = dynamodb.Table(name='Tokens')
+TABLE_NAME = 'Tokens'
+
 
 def create_table(dynamodb):
     table = dynamodb.create_table(
-        TableName = 'Tokens',
+        TableName = TABLE_NAME,
         KeySchema = [
             {
                 'AttributeName':'token',
@@ -22,10 +24,16 @@ def create_table(dynamodb):
         ],
         BillingMode = 'PAY_PER_REQUEST'
     )
+    sleep(7)
     return table
 
-def upload_credentials(table, token: str):
-    with open('credentials.json') as creds:
+def check_table_existence(dynamodb) -> bool:
+    tables = list(dynamodb.tables.all())
+    verified_tables = list(filter(lambda x: x.name == TABLE_NAME,tables))
+    return bool(verified_tables)
+
+def upload_credentials(table, path: str, token: str):
+    with open(path) as creds:
         data = json.load(creds)
 
     table.put_item(
@@ -34,3 +42,24 @@ def upload_credentials(table, token: str):
             'data':data
         }
     )
+
+    print("Successfully updated token")
+
+
+@click.command()
+@click.option('--file', '-f', 'file', required=True, help='path of the json file')
+@click.option('--token', '-t', 'token', required=True, help='token name')
+def main(file: str, token: str):
+    dynamodb = boto3.resource('dynamodb', region_name='sa-east-1')
+    if check_table_existence(dynamodb):
+        table = dynamodb.Table(name='Tokens')
+    else:
+        table = create_table(dynamodb)
+
+    print(table)
+
+    upload_credentials(table, file, token)
+
+
+if __name__ == '__main__':
+    main()
